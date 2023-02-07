b8bbcb0d9f76e6b8cda36148c74a213451208054
Fernanda Bess Gonçalves <fernanda-bess@hotmail.com>
2023-02-07 19:38:45 -0300

    first commit

---
+++ functions.js
@@ -0,0 +1,14 @@
+const loginInfos = {
+    userName: 'Fernanda',
+    emailAddress: 'fernanda-bess@hotmail.com',
+    password: 'senha1234'
+}
+
+login() {
+    const email = document.getElementById('emailAddress')
+    const pass = document.getElementById('password')
+
+    if (email == emailAddress && pass == password) {
+
+    }
+}
