43103ae5d420ba4f9e5dae791615355ff78ab1f2
Fernanda Bess Gonçalves <fernanda-bess@hotmail.com>
2023-02-09 18:04:05 -0300

    add function

--- functions.js
+++ functions.js
@@ -1,14 +1,25 @@
-const loginInfos = {
-    userName: 'Fernanda',
+var login = {
+    emailAddress: "",
+    password: ""
+}
+
+var loginCorreto = {
     emailAddress: 'fernanda-bess@hotmail.com',
     password: 'senha1234'
 }
 
-login() {
-    const email = document.getElementById('emailAddress')
-    const pass = document.getElementById('password')
+ let emailAddress = document.getElementById('emailAddress')
+ let password = document.getElementById('password')
+ let message = document.getElementById('message')
 
-    if (email == emailAddress && pass == password) {
-
-    }
-}
+function login() {
+    let checkEmail = emailAddress.value == loginCorreto.emailAddress;
+    let checkPassword = password.value == loginCorreto.password;
+  console.log(emailAddress.value) 
+  if (!checkEmail || !checkPassword) {
+        alert("Usuário ou senha incorretos!")
+    } else {
+        alert("Usuário correto. Logando...")
+        location.href="./main.html"
+    };
+} 
