2cd3bf848b9b3ff958a578f4b0318b536d69f16f
Fernanda Bess Gonçalves <fernanda-bess@hotmail.com>
2023-02-28 17:28:17 -0300

    first commit

--- functions.js
+++ functions.js
@@ -1,30 +1,22 @@
 
-let loginData = {
-    emailAddress: "",
-    password: ""
-}
-
-const loginCorreto = {
+const accountData = {
+    name: 'Fernanda Gonçalves',
     emailAddress: 'fernanda-bess@hotmail.com',
-    password: 'senha1234'
+    password: 'senha1234',
+    profilePicture: 'https://github.com/febessg'
 }
 
 let emailAddress = document.getElementById('emailAddress');
 let password = document.getElementById('password');
 let message = document.getElementById("message")
 
-/*function saveData() { 
-    localStorage.setItem('emailAddress', emailAddress.value);
-    localStorage.setItem('password', password.value);  
-} 
-    document.onchange = saveData();*/
-
 function login() {
-    let checkEmail = emailAddress.value == loginCorreto.emailAddress;
-    let checkPassword = password.value == loginCorreto.password;
+    let checkEmail = emailAddress.value == accountData.emailAddress;
+    let checkPassword = password.value == accountData.password;
 
-    localStorage.setItem('emailAddress', emailAddress.value);
-    localStorage.setItem('password', password.value); 
+    localStorage.setItem('name', accountData.name);
+    localStorage.setItem('emailAddress', accountData.emailAddress);
+    localStorage.setItem('password', accountData.password);localStorage.setItem('fotoDePerfil', accountData.profilePicture); 
 
   if (checkEmail && checkPassword) {
        alert("Usuário correto. Logando...")
@@ -33,6 +25,25 @@ function login() {
         message.innerHTML = "Usuário ou senha incorretos. Tente novamente!"
     }
 } 
+
+function changeInfo() {
+    let info = prompt("Qual informação você deseja alterar? [1] Foto de perfil, [2] Email, [3] Nome")
+    if (info == 1) {
+        localStorage.removeItem('fotoDePerfil')
+        let newPic = prompt("Digite a url da nova foto")
+        localStorage.setItem('fotoDePerfil', newPic)
+    } else if (info == 2) {
+        localStorage.removeItem('emailAddress');
+        let newEmail = prompt("Digite o novo email");
+        localStorage.setItem('emailAddress', newEmail)
+    } else if (info == 3) {
+        localStorage.removeItem('name');
+        let newName = prompt('Digite um nome');
+        localStorage.setItem('name', newName);
+    } else {
+        alert('Operação não reconhecida. Tente novamente.')
+    }
+}
 
 function logout() {
     location.href="./index.html"
